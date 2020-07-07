d1bd5267697cc8f159c608e1c8839024842fcc22
Sabrina Robinson <sabrinarob@gmail.com>
2020-07-07 14:26:20 -0600

    Add Article migrations

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,8 +1,8 @@
-require 'rails_helper'
+require "rails_helper"
 
-describe Article, type :model do
+describe Article, type: :model do
   describe "validations" do
-    it { should validate_presense_of(:title) }
-    it { should validate_presense_of(:body) }
+    it {should validate_presence_of(:title)}
+    it {should validate_presence_of(:body)}
   end
 end
