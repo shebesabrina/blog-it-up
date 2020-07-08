b408fc326c33f821e1f4b952c735c086172913a7
Sabrina Robinson <sabrinarob@gmail.com>
2020-07-08 11:31:48 -0600

    TEST- Article Edit action

--- spec/features/article_feature_spec.rb
+++ spec/features/article_feature_spec.rb
@@ -64,4 +64,21 @@ describe "user sees all articles" do
       expect(page).to_not have_content(article_1.title)
     end
   end
+
+  describe "user can edit an article" do
+    it "updates the article by id" do
+      article = Article.create!(title: "Title 1", body: "Body 1")
+
+      visit article_path(article)
+      click_link "Edit"
+
+      fill_in "article[title]", with: "New Title!"
+      fill_in "article[body]", with: "New Body!"
+      click_on "Submit"
+
+      expect(current_path).to eq(articles_path)
+      expect(page).to have_content("New Title!")
+      expect(page).to have_content("New Body!")
+    end
+  end
 end
