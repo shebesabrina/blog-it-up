e582bcf5eb9f7832fdf9f73c4aff3a170c6b780e
Sabrina Robinson <sabrinarob@gmail.com>
2020-07-08 08:48:23 -0600

    TEST - Implements testing for new Article action

--- spec/features/article_feature_spec.rb
+++ spec/features/article_feature_spec.rb
@@ -34,4 +34,20 @@ describe "user sees all articles" do
       expect(page).to_not have_content(article_2.title)
     end
   end
+
+  describe "user creates a new article" do
+    it "creates a new article" do
+      visit articles_path
+      click_link "Create a New Article"
+
+      expect(current_path).to eq(new_article_path)
+
+      fill_in "article[title]", with: "New Title!"
+      fill_in "article[body]", with: "New Body!"
+      click_on "Create Article"
+
+      expect(page).to have_content("New Title!")
+      expect(page).to have_content("New Body!")
+    end
+  end
 end
