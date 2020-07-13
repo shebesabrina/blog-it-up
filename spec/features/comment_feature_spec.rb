c96762427d3f2740be3c1830ad662390edfdd5a3
Sabrina Robinson <sabrinarob@gmail.com>
2020-07-13 17:47:28 -0600

    ATP - Implements Comment view form and create action

--- spec/features/comment_feature_spec.rb
+++ spec/features/comment_feature_spec.rb
@@ -15,6 +15,22 @@ describe 'user sees one article' do
     expect(page).to have_content(comment_1.author_name)
     expect(page).to have_content(comment_1.body)
     expect(page).to have_content(comment_2.author_name)
-    expect(page).to have_content(comment_2.body)  
+    expect(page).to have_content(comment_2.body)
+  end
+
+  describe 'user can fill in a comment form' do
+    it 'displays the comment in the article show' do
+      article =  Article.create(title: 'New Title', body: 'New Body')
+
+      visit article_path(article)
+
+      fill_in "comment[author_name]", with: 'ME!'
+      fill_in "comment[body]", with: "chit-chat"
+      click_on 'Submit'
+
+      expect(current_path).to eq(article_path(article))
+      expect(page).to have_content('ME!')
+      expect(page).to have_content('chit-chat')
+    end
   end
 end
